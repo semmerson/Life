a822f72a9100cd6d28e57f02c225f3efab7d5b3a
Steven Emmerson <steve@unidata.ucar.edu>
2013-08-09 13:44:10 -0600

    Provision VM with gradle.

---
+++ manifests/init.pp
@@ -0,0 +1,24 @@
+group { "puppet":
+  ensure => "present",
+}
+
+File { owner => 0, group => 0, mode => 0644 }
+
+file { '/etc/motd':
+  content => "Welcome to your Vagrant-built virtual machine!
+              Managed by Puppet.\n"
+}
+
+Exec { path => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'], }
+
+exec {'bashrc':
+  command => 'echo "set -o vi" >>/home/vagrant/.bashrc',
+  unless  => 'grep "set *-o" /home/vagrant/.bashrc',
+}
+  
+exec {'update': command => 'apt-get update', }
+
+package { 'gradle':
+  ensure  => present,
+  require => Exec['update'],
+}
